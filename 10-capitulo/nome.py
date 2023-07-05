17308345fda8255d098b1b12d88b14727bca2ba4
Vitor Mateus <vmateusaguiar@gmail.com>
2023-07-05 14:48:40 -0300

    feat: property e setter

--- 10-capitulo/nome.py
+++ 10-capitulo/nome.py
@@ -1,15 +1,15 @@
+from functools import total_ordering
+
+@total_ordering
 class Nome:
     def __init__(self, nome):
-        if nome == None or not nome.strip():
-            raise ValueError('Nome não pode ser nulo nem em branco')
         self.nome = nome
-        self.chave = nome.strip().lower()
 
     def __str__(self):
         return self.nome
     
     def __repr__(self):
-        return f'<Classe {type(self).__name__} em 0x{id(self):x} Nome: {self.nome} Chave: {self.chave}>'
+        return f'<Classe {type(self).__name__} em 0x{id(self):x} Nome: {self.__nome} Chave: {self.__chave}>'
 
     def __eq__(self, outro):
         print('__eq__ Chamado')
@@ -18,3 +18,18 @@ class Nome:
     def __lt__(self, outro):
         print('__lt__ Chamado')
         return self.nome < outro.nome
+    
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @nome.setter
+    def nome(self, valor):
+        if valor == None or not valor.strip():
+            raise ValueError('Nome não pode ser nulo nem em  branco')
+        self.__nome = valor
+        self.__chave = Nome.CriaChave(valor)
+    
+    @staticmethod
+    def CriaChave(nome):
+        return nome.strip().lower()
